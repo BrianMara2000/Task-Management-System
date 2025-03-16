456f6b370f0bf775a3d432ff49a4f17672554201
Brian Mara <brian@blendit.eu>
2025-03-16 22:52:02 +0800

    Refactor LoginForm component to use react-hook-form for form handling, add password visibility toggle, and improve error handling

--- frontend/src/features/auth/components/LoginForm.jsx
+++ frontend/src/features/auth/components/LoginForm.jsx
@@ -1,59 +1,97 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUser, setToken } from "../authSlice";
-import axiosClient from "../../../axios";
-import { Mail, LockKeyhole } from "lucide-react";
+import { axiosClient, csrfClient } from "../../../axios";
+import { Mail, LockKeyhole, Eye, EyeOff } from "lucide-react";
 import LoginIllustrator from "../../../assets/LoginIllustrator.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import Spinner from "@/components/ui/spinner";
+
+const schema = z.object({
+  email: z.string().email("This field is required"),
+  password: z.string().min(6, "This field is required"),
+});
 
 const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError(null);
-
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    setIsLoading((prev) => !prev);
     try {
-      await axiosClient.get("/sanctum/csrf-cookie"); // CSRF protection
+      await csrfClient.get("/sanctum/csrf-cookie"); // CSRF protection
       const response = await axiosClient.post("/login", {
-        email,
-        password,
+        email: data.email,
+        password: data.password,
       });
 
       dispatch(setUser({ user: response.data.user }));
       dispatch(setToken(response.data.token));
+
+      localStorage.setItem("token", response.data.token);
+      navigate("/app/home");
     } catch (err) {
-      setError(err.response?.data?.message || "Login failed");
+      if (err.response?.status === 422) {
+        const backendErrors = err.response.data.errors;
+        Object.keys(backendErrors).forEach((key) => {
+          setError(key, {
+            type: "server",
+            message: backendErrors[key][0],
+          });
+        });
+      } else {
+        setError("email", {
+          type: "manual",
+          message: err.response?.data?.message || "Login failed",
+        });
+      }
+    } finally {
+      setIsLoading((prev) => !prev);
     }
   };
 
   return (
     <div className="flex h-screen items-center justify-center w-[50%]">
-      <div className="bg-white rounded-xl shadow-lg flex w-full h-[50%]">
+      <div className="bg-white rounded-xl shadow-lg flex w-full min-h-[50%]">
         {/* Left Section */}
         <div className="w-1/2 p-8">
           <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             {/* Email */}
             <div>
               <div className="flex items-center border-b border-gray-300 py-2">
                 <Mail
                   strokeWidth={1.5}
-                  className="w-6 h-6 text-white fill-purple-600 mr-5"
+                  className="w-6 h-6 text-white fill-purple-600 mr-2"
                 />
-                <input
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0 "
                   type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full focus:outline-none"
-                  required
                   placeholder="Enter your email"
+                  {...register("email")}
                 />
               </div>
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
             {/* Password */}
@@ -61,20 +99,28 @@ const LoginForm = () => {
               <div className="flex items-center border-b border-gray-300 py-2">
                 <LockKeyhole
                   strokeWidth={1.5}
-                  className="w-6 h-6 text-white fill-purple-600 mr-5"
+                  className="w-6 h-6 text-white fill-purple-600 mr-2"
                 />
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full focus:outline-none"
-                  required
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0 "
+                  type={showPassword ? "text" : "password"}
                   placeholder="Enter your password"
+                  {...register("password")}
                 />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="ml-2 focus:outline-none cursor-pointer"
+                >
+                  {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                </button>
               </div>
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
-
-            {error && <p style={{ color: "red" }}>{error}</p>}
 
             {/* Forgot Password */}
             <div className="text-right">
@@ -84,12 +130,16 @@ const LoginForm = () => {
             </div>
 
             {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 transition"
-            >
-              Login
-            </button>
+            <Button type="submit" className="w-full" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <Spinner className="w-4 h-4 border-2 border-t-white border-gray-300 rounded-full animate-spin mr-2" />
+                  Loading...
+                </>
+              ) : (
+                "Login"
+              )}
+            </Button>
           </form>
 
           {/* Signup Link */}
--- frontend/src/features/auth/components/RegisterForm.jsx
+++ frontend/src/features/auth/components/RegisterForm.jsx
@@ -1,59 +1,134 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import { useDispatch } from "react-redux";
 import { setUser, setToken } from "../authSlice";
-import axiosClient from "../../../axios";
-import { Mail, LockKeyhole } from "lucide-react";
+import { axiosClient, csrfClient } from "../../../axios";
+import {
+  Mail,
+  LockKeyhole,
+  LockKeyholeOpen,
+  UserRound,
+  Eye,
+  EyeOff,
+} from "lucide-react";
 import RegisterIllustrator from "../../../assets/RegisterIllustrator.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+const schema = z
+  .object({
+    name: z.string().nonempty("Name is required"),
+    email: z.string().email("Invalid email"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 const RegisterForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
   const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError(null);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
+  // ✅ Handle Form Submission
+  const onSubmit = async (data) => {
     try {
-      await axiosClient.get("/sanctum/csrf-cookie"); // CSRF protection
-      const response = await axiosClient.post("/login", {
-        email,
-        password,
+      await csrfClient.get("/sanctum/csrf-cookie");
+
+      const response = await axiosClient.post("/register", {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        password_confirmation: data.confirmPassword,
       });
 
       dispatch(setUser({ user: response.data.user }));
       dispatch(setToken(response.data.token));
+
+      navigate("/app/home");
     } catch (err) {
-      setError(err.response?.data?.message || "Login failed");
+      if (err.response?.status === 422) {
+        // ✅ Handle backend validation errors
+        const backendErrors = err.response.data.errors;
+        Object.keys(backendErrors).forEach((key) => {
+          setError(key, {
+            type: "server",
+            message: backendErrors[key][0], // Show first error only
+          });
+        });
+      } else {
+        setError("email", {
+          type: "manual",
+          message: err.response?.data?.message || "Registration failed",
+        });
+      }
     }
   };
 
   return (
     <div className="flex h-screen items-center justify-center w-[50%]">
-      <div className="bg-white rounded-xl shadow-lg flex flex-row-reverse w-full h-[50%]">
+      <div className="bg-white rounded-xl shadow-lg flex flex-row-reverse w-full min-h-[50%]">
         {/* Left Section */}
         <div className="w-1/2 p-8">
-          <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+          <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            {/* Name */}
+            <div>
+              <div className="flex items-center border-b border-gray-300 py-2">
+                <UserRound
+                  strokeWidth={1.5}
+                  className="w-6 h-6 text-white fill-purple-600 mr-2"
+                />
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0"
+                  type="text"
+                  placeholder="Enter your name"
+                  {...register("name")}
+                />
+              </div>
+              {errors.name && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+
             {/* Email */}
             <div>
               <div className="flex items-center border-b border-gray-300 py-2">
                 <Mail
                   strokeWidth={1.5}
-                  className="w-6 h-6 text-white fill-purple-600 mr-5"
+                  className="w-6 h-6 text-white fill-purple-600 mr-2"
                 />
-                <input
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0 "
                   type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full focus:outline-none"
-                  required
                   placeholder="Enter your email"
+                  {...register("email")}
                 />
               </div>
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
             {/* Password */}
@@ -61,35 +136,67 @@ const RegisterForm = () => {
               <div className="flex items-center border-b border-gray-300 py-2">
                 <LockKeyhole
                   strokeWidth={1.5}
-                  className="w-6 h-6 text-white fill-purple-600 mr-5"
+                  className="w-6 h-6 text-white fill-purple-600 mr-2"
                 />
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full focus:outline-none"
-                  required
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0 "
+                  type={showPassword ? "text" : "password"}
                   placeholder="Enter your password"
+                  {...register("password")}
                 />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="ml-2 focus:outline-none cursor-pointer"
+                >
+                  {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                </button>
               </div>
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
 
-            {error && <p style={{ color: "red" }}>{error}</p>}
-
-            {/* Forgot Password */}
-            <div className="text-right">
-              <a href="#" className="text-purple-500 text-sm hover:underline">
-                Forgot Password?
-              </a>
+            {/* Confirm Password */}
+            <div>
+              <div className="flex items-center border-b border-gray-300 py-2">
+                <LockKeyholeOpen
+                  strokeWidth={1.5}
+                  className="w-6 h-6 text-white fill-purple-600"
+                />
+                <Input
+                  className="border-none focus-visible:border-none focus:ring-0 focus-visible:ring-0 focus-visible:border-0 "
+                  type={showPasswordConfirmation ? "text" : "password"}
+                  placeholder="Confirm your password"
+                  {...register("confirmPassword")}
+                />
+                <button
+                  type="button"
+                  onClick={() =>
+                    setShowPasswordConfirmation(!showPasswordConfirmation)
+                  }
+                  className="ml-2 focus:outline-none cursor-pointer"
+                >
+                  {showPasswordConfirmation ? (
+                    <Eye size={20} />
+                  ) : (
+                    <EyeOff size={20} />
+                  )}
+                </button>
+              </div>
+              {errors.confirmPassword && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.confirmPassword.message}
+                </p>
+              )}
             </div>
 
             {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 transition"
-            >
-              Login
-            </button>
+            <Button type="submit" className="w-full">
+              Register
+            </Button>
           </form>
 
           {/* Signup Link */}
