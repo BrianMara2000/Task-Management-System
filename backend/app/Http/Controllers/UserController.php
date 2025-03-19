8c7fc75575fd4aa0d1e91d1060f95e08e0c82257
Brian Mara <brian@blendit.eu>
2025-03-19 21:30:42 +0800

    Implement UserController with CRUD methods and add UserResource for user data transformation

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,11 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    public function show(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $perPage = request('per_page', 10);
+        $users = User::paginate($perPage);
+
+        return response()->json(['users' => $users]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function getUser(Request $request)
     {
         return response()->json($request->user());
     }
