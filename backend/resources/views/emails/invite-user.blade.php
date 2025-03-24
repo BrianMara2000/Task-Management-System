736e184c644abd3f07aa966daaee38d1c45f254f
Brian Mara <brian@blendit.eu>
2025-03-24 22:06:12 +0800

    Add user invitation feature with email sending and token management

---
+++ backend/resources/views/emails/invite-user.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Invitation to Join</title>
+</head>
+<body>
+    <h2>You are invited to join our platform!</h2>
+    <p>Click the link below to accept the invitation:</p>
+    <a href="{{ $inviteLink }}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none;">Accept Invitation</a>
+    <p>If you didn't request this, please ignore this email.</p>
+</body>
+</html>
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,14 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InviteUserController;
 use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/accept-invite', [InviteUserController::class, 'acceptInvite']);
 Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::get('/getUser', [UserController::class, 'getUser']);
   Route::post('/logout', [AuthController::class, 'logout']);
 
   Route::apiResource('/users', UserController::class);
+
+  Route::post('users/invite-user', [InviteUserController::class, 'inviteUser']);
 });
