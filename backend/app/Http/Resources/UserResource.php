bdb20d89447569152e835f68393e5e7436d54994
Brian Mara <brian@blendit.eu>
2025-03-25 22:39:44 +0800

    Refactor UserResource to format created_at date and add ProjectController routes

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,9 +21,8 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'role' => $this->role,
             'profile_image' => $this->profile_image,
-            'created_at' => $this->created_at,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
         ];
     }
 }
