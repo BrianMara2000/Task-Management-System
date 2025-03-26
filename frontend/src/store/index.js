b5b53fac9283bb2233d172f60b590be1c297f191
Brian Mara <brian@blendit.eu>
2025-03-26 23:54:33 +0800

    Add project reducer to Redux store for project state management

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import userReducer from "../features/user/userSlice";
+import projectReducer from "../features/project/projectSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    project: projectReducer,
   },
 });
