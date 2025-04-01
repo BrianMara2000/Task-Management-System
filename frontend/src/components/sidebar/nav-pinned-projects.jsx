b8fa732fe1194938d0d22a2964d18bf8c5e55a0e
Brian Mara <brianmara123@gmail.com>
2025-04-01 23:48:13 +0800

    Add delete project functionality and enhance pinned projects UI

--- frontend/src/components/sidebar/nav-pinned-projects.jsx
+++ frontend/src/components/sidebar/nav-pinned-projects.jsx
@@ -1,4 +1,10 @@
-import { Folder, Forward, MoreHorizontal, Trash2 } from "lucide-react";
+import {
+  Folder,
+  Forward,
+  MoreHorizontal,
+  Trash2,
+  Trash2Icon,
+} from "lucide-react";
 
 import {
   DropdownMenu,
@@ -16,11 +22,58 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { useNavigate } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Link, useNavigate } from "react-router-dom";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { getColorFromName } from "@/constants/constants";
+import { useState } from "react";
+import { axiosClient } from "@/axios";
+import { useDispatch } from "react-redux";
+import {
+  deleteProject as deleteProjectAction,
+  setProject,
+} from "@/features/project/projectSlice";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
 
 export function NavPinnedProjects({ pinnedProjects }) {
   const { isMobile } = useSidebar();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(null);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const deleteProject = async (id) => {
+    setIsLoading(true);
+    setIsDeleteOpen(true);
+    try {
+      await axiosClient.delete(`/projects/${id}`);
+      dispatch(deleteProjectAction(id));
+      dispatch(setProject(null));
+      setIsDeleteOpen(false);
+      toast("Project Deleted", {
+        description: "The project has been removed successfully.",
+        type: "success",
+      });
+    } catch (error) {
+      toast("Error", {
+        description: "Failed to delete the project. Please try again.",
+        type: "error",
+      });
+      console.error("Failed to delete project:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
@@ -29,14 +82,23 @@ export function NavPinnedProjects({ pinnedProjects }) {
         {pinnedProjects.map((item) => (
           <SidebarMenuItem key={item.id}>
             <SidebarMenuButton asChild>
-              <a href="#">
-                <img
-                  src={item.image_path}
-                  alt="Project Image"
-                  className="w-5 h-5 rounded-full"
-                />
+              <Link to={`/app/projects/${item.id}`}>
+                <Avatar className="rounded w-14 h-10">
+                  <AvatarImage
+                    className="rounded w-14 h-10"
+                    src={item.image_path}
+                    alt="Project Image"
+                  />
+                  <AvatarFallback
+                    className={`rounded w-14 h-10 ${getColorFromName(
+                      item.name
+                    )} text-white font-bold`}
+                  >
+                    {item.name.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>
                 <span>{item.name}</span>
-              </a>
+              </Link>
             </SidebarMenuButton>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -51,8 +113,13 @@ export function NavPinnedProjects({ pinnedProjects }) {
                 align={isMobile ? "end" : "start"}
               >
                 <DropdownMenuItem>
-                  <Folder className="text-muted-foreground" />
-                  <span>View Project</span>
+                  <Link
+                    to={`/app/projects/${item.id}`}
+                    className="flex items-center justify-center gap-2"
+                  >
+                    <Folder className="text-muted-foreground" />
+                    <span>View Project</span>
+                  </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
                   <Forward className="text-muted-foreground" />
@@ -60,8 +127,41 @@ export function NavPinnedProjects({ pinnedProjects }) {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  <Trash2 className="text-muted-foreground" />
-                  <span>Delete Project</span>
+                  <Link className="flex items-center justify-center gap-2">
+                    <Dialog open={isDeleteOpen} onOpenChange={setIsDeleteOpen}>
+                      <DialogTrigger asChild>
+                        <button className="flex items-center justify-center gap-2">
+                          <Trash2Icon className="text-muted-foreground" />
+                          <span>Delete Project</span>
+                        </button>
+                      </DialogTrigger>
+
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Confirm Deletion</DialogTitle>
+                          <DialogDescription>
+                            Are you sure you want to delete this project? This
+                            action cannot be undone.
+                          </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                          <Button
+                            variant="outline"
+                            onClick={() => setIsDeleteOpen(false)}
+                          >
+                            Cancel
+                          </Button>
+                          <Button
+                            className="bg-red-500 rounded-md shadow-md"
+                            onClick={() => deleteProject(item.id)}
+                            disabled={isLoading}
+                          >
+                            {isLoading ? "Deleting..." : "Delete"}
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </Link>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
