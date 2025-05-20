5578f9213f271bd8886e68beafb3c60356a526d3
Brian Mara <brianmara123@gmail.com>
2025-05-20 22:40:45 +0800

    Implement CommentController methods for retrieving and storing comments with resource transformation

---
+++ backend/app/Http/Resources/CommentResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public static $wrap = false;
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'task_id' => $this->task_id,
+            'created_by' => $this->created_by,
+            'comment' => $this->content,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'updated_at' => (new Carbon($this->updated_at))->format('Y-m-d'),
+        ];
+    }
+}
