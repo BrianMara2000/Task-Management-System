d4e936f6ea77c265afc23fd1410609be4559a98c
Brian Mara <brianmara123@gmail.com>
2025-06-03 22:14:22 +0800

    Remove unused user state and related effect in App component for cleaner code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import AppRoutes from "./routes/AppRoutes";
 import "./index.css";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setUser, setToken } from "@/features/auth/authSlice";
 import { axiosClient } from "./axios";
 
 const App = () => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
-  const user = useSelector((state) => state.auth.user);
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
@@ -32,10 +32,6 @@ const App = () => {
 
     fetchUser();
   }, [dispatch]);
-
-  useEffect(() => {
-    console.log("user: ", user);
-  }, [user]);
 
   if (loading) return <div>Loading...</div>; // Optional: Add a loading indicator
 
